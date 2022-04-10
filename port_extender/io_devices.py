d8bb89d2ed053df10cb6255f1e20fd4b7cd40103
John Mauzey <john@mauzey.com>
2022-04-09 20:56:33 -0600

    Adding functionality to control hardware port extenders.

---
+++ port_extender/io_devices.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# 20220314 jfm port_exapnder.py
+
+# Python 2/3 compatibility imports
+from __future__ import print_function
+import gv  # Get access to SIP's settings, gv = global variables
+
+# import smbus required to control the io expander hardware
+blockedPlugin = False # assume that the needed module is available
+try:
+    import smbus
+except ModuleNotFoundError:
+    try:
+        import smbus2 as smbus
+    except ModuleNotFoundError:
+        blockedPlugin = True  # missing smbus module
+
+class IO_Extender():
+    '''This is the base class for all supported io_extender hardware.'''
+    def __init__(self, bus_id=1, ic_type="mcp23017", bus_addr=0x20,
+                 initialize=False, alr=False):
+        '''Must configure port hardwre. The mcp family must initialize the
+        Data Direction Register (DDR) to set all ports as outputs. The pcf
+        hardware has no DDR to control the port behavior. The pcf devices
+        have a small current source that pulls the output high similar to
+        a weak pullup on an open collector output. This interface only
+        supports using the io extenders as outputs. Initialization needs
+        only to be done once.'''
+        self._bus_id = bus_id
+        self._ic_type = ic_type
+        self._bus_addr = bus_addr
+        self._alr = alr
+
+    def set_output(self, val):
+        pass
+
+class MCP23017(IO_Extender):
+    '''The mcp23017 has 16 outputs that are capable of sinking or sourcing
+    up to 25 mA each making it suitable to drive most relays regardless of
+    whether the control logic is active high or active low.'''
+    def __init__(self, bus_id=1, bus_addr=0x20, initialize = False, alr = False):
+        super().__init__()
+        self._bus = smbus.SMBus(self._bus_id)
+        if initialize:
+            #program DDR to all outputs and set outputs to low unless alr==True
+            pass
+
+    def set_output(self, val):
+        if gv.sd[u"alr"] == 0:
+            val = ~val & 0xffff
+        else:
+            val = val & 0xffff
+        print('MCP23017: setting output to 0x{:04X}'.format(val))
+
+class MCP2308(IO_Extender):
+    '''The mcp2308 has 8 outputs that are capable of sinking or sourcing
+    up to 25 mA each making it suitable to drive most relays regardless of
+    whether the control logic is active high or active low.'''
+    def __init__(self, bus_id=1, bus_addr=0x20, initialize = False, alr = False):
+        super().__init__()
+        self._bus = smbus.SMBus(self._bus_id)
+        if initialize:
+            #program DDR to all outputs and set outputs to low unless alr==True
+            pass
+
+    def set_output(self, val):
+        if gv.sd[u"alr"] == 0:
+            val = ~val & 0xff
+        else:
+            val = val & 0xff
+        print('MCP2308: setting output to 0x{:02X}'.format(val))
+
+class PCF8575(IO_Extender):
+    '''The PCF8575 has 16 outputs that are capable of sinking
+    up to 15 mA each making it suitable to drive most relays.'''
+    def __init__(self, bus_id=1, bus_addr=0x20, initialize = False, alr = False):
+        super().__init__()
+        if initialize:
+            #initialize outputs to high weakly driven
+            pass
+
+    def set_output(self, val):
+        if gv.sd[u"alr"] == 0:
+            val = ~val & 0xffff
+        else:
+            val = val & 0xffff
+        print('PCF8575: setting output to 0x{:04X}'.format(val))
+
+class PCF8574(IO_Extender):
+    '''The PCF8574 has 8 outputs that are capable of sinking
+    up to 15 mA each making it suitable to drive most relays.'''
+    def __init__(self, bus_id=1, bus_addr=0x20, initialize = False, alr = False):
+        super().__init__()
+        self._bus = smbus.SMBus(self._bus_id)
+        if initialize:
+            #initialize outputs to high weakly driven
+            pass
+
+    def set_output(self, val):
+        if gv.sd[u"alr"] == 0:
+            val = ~val & 0xff
+        else:
+            val = val & 0xff
+        print('pcf8574: setting output to 0x{:02X}'.format(val))
+
+def Devices(bus_id=1, ic_type="pcf8574", bus_addr=0x20,
+            initialize=False, alr=False):
+    '''This is a factory to create the interface to the io extender.'''
+    if ic_type == "mcp23017":
+        return(MCP23017(bus_id, bus_addr, initialize, alr))
+    elif ic_type == "mcp2308":
+        return (MCP2308(bus_id, bus_addr, initialize, alr))
+    elif ic_type == "pcf8575":
+        return (PCF8575(bus_id, bus_addr, initialize, alr))
+    elif ic_type == "pcf8574":
+        return (PCF8574(bus_id, bus_addr, initialize, alr))
+    else:
+        print(u"ERROR: PEX unsupported device type requested {}".format(ic_type))
---
+++ port_extender/port_extender.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+# 20220314 jfm port_extender.py
+
+# Python 2/3 compatibility imports
+from __future__ import print_function
+
+# standard library imports
+import json
+
+import gv  # Get access to SIP's settings, gv = global variables
+from io_devices import Devices
+
+# import smbus required to control the io port hardware
+blockedPlugin = False # assume that the needed module is available
+try:
+    import smbus
+except ModuleNotFoundError:
+    try:
+        import smbus2 as smbus
+    except ModuleNotFoundError:
+        blockedPlugin = True  # missing smbus module
+
+pex_config = {}
+
+# smbus tool
+def i2c_scan(i2c_bus, start_addr = 0x08, end_addr = 0xF7):
+    devices_discovered = []
+    for i in range(start_addr, end_addr+1):
+        try:
+            i2c_bus.write_quick(i)
+            devices_discovered.append(i)
+        except OSError as e:
+            pass  # no device responded
+    return devices_discovered  # list of addresses from successful handshake ACK
+
+
+class PEX():
+
+    def __init__(self):
+        global pex_config
+        try:
+            self._number_of_stations = len(gv.srvals)
+            self.load_pex_config()
+            self._dev_configs = pex_config['dev_configs']  # list of preconfigured device(s)
+            self._discovered_devices = []
+            self._debug = pex_config['debug']
+        except (TypeError, KeyError) as e:
+            self._pex_config = self.create_default_config()
+            self._dev_configs = self._pex_config[u"dev_configs"]
+            self._debug = True
+            print(u'ERROR: PEX bad or missing hardware config')
+            print(e)
+
+    def create_default_config(self):
+        return {u"pex_status": u"unconfigured",
+                   u"warnmsg": '',
+             u"default_smbus": 1,
+               u"dev_configs": [{u"bus_id": 1,
+                                u"hw_addr": 0x23,
+                                   u"size": 16,
+                                   u'first': 0,
+                                    u'last': 0,
+                                 u"ic_type": "mcp23017"
+                               }],
+                u"discovered_devices": [],
+                u"dev_adr": [0x20],  # for testing pex_conf integration
+                u"supported_hardware": ['pcf8574', 'pcf8575', 'mcp2308', 'mcp23017'],
+                u"ic_type": 'pcf8575',
+                u"debug": "0"
+        }
+
+        # Read in the pex config for this plugin from it's JSON file
+    def load_pex_config(self):
+        global pex_config
+        try:
+            with open(u"./data/pex_config.json", u"r") as f:
+                pex_config = json.load(f)  # Read the pex_config from file
+        except IOError:  # If file does not exist create file with defaults.
+            pex_config = self.create_default_config()
+            with open(u"./data/pex_config.json", u"w") as f:
+                json.dump(pex_config, f, indent=4)
+        return
+
+
+    def scan_for_ioextenders(self, bus_id):
+        'Scan well known bus address range for supported hardware port extenders.'
+        i2c_bus = smbus.SMBus(int(bus_id))
+        i2c_start_addr = 0x20  # beginning i2c address for MCP23017 and pcf857x
+        i2c_end_addr = 0x27  # last possible i2c address for MCP23017 and pcf857x
+        self._discovered_devices = i2c_scan(i2c_bus, i2c_start_addr, i2c_end_addr)
+        return self._discovered_devices
+
+
+    def verify_device_handshake(self, bus_id, bus_addr: int):  # jfm static typing
+        'Use SMbus ACK protocol for handshake to verify connectivity.'
+        i2c_bus = smbus.SMBus(int(bus_id))
+        i2c_start_addr = bus_addr  #  device to verify
+        i2c_end_addr = bus_addr
+        return (i2c_scan(i2c_bus, i2c_start_addr, i2c_end_addr))
+
+    def alter_SIP_gpio_behavior(self):
+        'Disable SIP gpio shift register if Port Extender is configured to use smbus.'
+        if len(self._dev_configs):  # at least one interface board is configured
+            # disable gpio_pins. We can discuss later if a mix of gpio and i2c should be possible
+            gv.use_gpio_pins = False
+        else:
+            gv.use_gpio_pins = True
+
+    def has_config_changed(self, conf):
+        return self._dev_configs != conf['dev_configs'] or \
+               self._number_of_stations != len(gv.srvals)
+
+    def set_output(self, conf):
+        'Maps the SIP Station Values to the configured hardware port(s).'
+        global pex_config
+        if self.has_config_changed(conf):
+            print(u"ERROR: PEX configuration changed. Need to reconfigure.")
+            return
+        elif pex_config['pex_status'] != "run":
+            print(u'ERROR: PEX not in "run" mode. Need to reconfigure.')
+            return
+
+        # now use srvalues to set values of configured ports
+        print("DeBug: PEX set outputs for {} ports.".format(len(gv.srvals)))
+        # Map the srvalues to the device(s). The order that the devices are
+        # listed in the config are the order for mapping. The first device
+        # maps the first DeviceSize (8 or 16) ports to Station_1 through
+        # Station_N (n=8 or 16).
+        st = 0  # start index in successive slices
+        sr_len = len(gv.srvals)
+        device_count = 0  # jfm for debug track which device is selected
+        for dev in self._dev_configs:
+            device_count += 1
+            port_size = dev[u"size"]
+            hw_addr = dev["hw_addr"]
+            bus_id = dev[u"bus_id"]
+            ic_type = dev[u"ic_type"]
+            result = 0
+            n = 1
+            stp = min(st + port_size, sr_len)  # last element in slice
+            for i in range(st, stp):
+                if gv.srvals[i] == 1:
+                    result += n
+                n *= 2  # arithmetic shift left one bit
+            st = st + port_size  # ready for next slice
+            if st > sr_len:
+                print(f'Debug: PEX dev@0x{hw_addr:02X} has {st-sr_len} unused ports.')
+                break
+            port = Devices(bus_id, ic_type, hw_addr, alr=False)
+            port.set_output(result)
+            print("Debug: PEX set_output to {:04X} for device {}".format(result,device_count))
+        if st < sr_len:  # ran out of devices to map before last srvals have been output
+            print(u"ERROR: PEX too many Stations configured for configured io_extender hardware.")
+            print(u"           Either reduce number of stations are add/configure additional")
+            print(u"           io_extender hardware.")
+
